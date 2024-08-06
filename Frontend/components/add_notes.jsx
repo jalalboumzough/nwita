4c0f7c21b57eaba46d776afbde7882316542d44e
jalalboumzough <jalal.boumzough@gmail.com>
2024-08-06 16:27:00 +0100

    Updtate the day

---
+++ Frontend/components/add_notes.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './add_notes.css'
+
+export default function add_notes() {
+  return (
+    <div className='Add_note_div'>
+        <from>
+            <input type="text" placeholder='Note title'></input>
+            <input type="text" placeholder='Object'></input>
+            <textarea name="" placeholder='Content'></textarea>
+        </from>
+    
+    </div>
+  )
+}
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import LoginForm from '../components/login'
 import SignUp from '../components/SignUp.jsx'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Notepost from '../components/note_post.jsx';
+import AddNote from '../components/add_notes.jsx';
 
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
     <Routes>
     <Route path="/" element={<LoginForm />} />
     <Route path="/signup" element={<SignUp />} />
+    <Route path="/notepost" element={<Notepost />} />
+    <Route path="/addnote" element={<AddNote />} />
     </Routes>
 
     </Router>
